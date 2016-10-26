d307bf761aa238604755a620592913d24a0f6028
Tristan Lorach <lorachnroll@gmail.com>
2016-10-26 14:56:29 -0700

    to push all

---
+++ push_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# walk through downloaded samples and pull for latest changes
+# Note: doesn't take care of possible conflicts
+pushd ..
+for dir in *; do
+   for file in "$dir"; do
+     pushd $file
+     git push
+     popd
+   done
+done
+popd
